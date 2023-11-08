d56b9d24b702a26542cc4981a133f14ca5b4b234
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-08 21:15:33 +0900

    Title: 기수 정렬 구현, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231108/기수 정렬 구현/implement-radix-sort.cpp
+++ 231108/기수 정렬 구현/implement-radix-sort.cpp
@@ -11,14 +11,7 @@ int main() {
         if(arr[i]>MAX)MAX=arr[i];
     }
 
-    int cur=100000,k=6;
-    while(1){
-        if(MAX/cur>=1) break;
-        cur/=10;
-        k--;
-    }
-
-    for(int i=0;i<k;i++){
+    for(int i=0;i<6;i++){
         vector<int> temp[10];
         int d = 1;
         for(int j=0;j<n;j++){

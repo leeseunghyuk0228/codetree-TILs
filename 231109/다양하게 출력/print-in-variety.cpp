af9dbf52ca0421d32d7693c374735daea920f3d8
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-09 23:12:04 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/다양하게 출력/print-in-variety.cpp
+++ 231109/다양하게 출력/print-in-variety.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout<<"Total days in Year\n365\nCircumference rate\n3.1415926535"
+    cout<<"Total days in Year\n365\nCircumference rate\n3.1415926535";
     return 0;
 }

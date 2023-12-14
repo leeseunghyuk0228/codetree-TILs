6801f00e0776ccb6154d77688898f893015a89ee
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-12-14 13:49:22 +0900

    Title: 술래잡기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231214/술래잡기/hide-and-seek.cpp
+++ 231214/술래잡기/hide-and-seek.cpp
@@ -164,7 +164,7 @@ void solution(int t) {
 
 int main()
 {
-    sync_with_stdio(false);cin.tie(0);
+    ios_base::sync_with_stdio(false);cin.tie(0);
     input();
     for (int i = 1; i <=k; i++) solution(i);
     cout << res;
